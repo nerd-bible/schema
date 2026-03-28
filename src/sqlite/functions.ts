10f4054379b9f0902382d137640bb7cb1e981d69
thesmartwon <thesmartwon@pm.me>
2026-03-28 11:35:21 -0400

    please typescript

--- src/sqlite/functions.ts
+++ src/sqlite/functions.ts
@@ -2,7 +2,7 @@
 import * as stemmers from "../stemmers/index.ts";
 
 type SQLValue = null | number | bigint | string;
-export function stemmer(lang: SQLValue, text: SQLValue): SQLValue {
+export function stemmer(lang: any, text: any): SQLValue {
 	if (typeof text !== "string") return null;
 
 	if (lang === "eng") return stemmers.eng(text);
